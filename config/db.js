df8027e04c6f5470fd3e08188ef5d8603a7f209b
Marcos Noble <marcos.luz.noble@gmail.com>
2024-06-26 16:33:08 -0300

    .env

--- config/db.js
+++ config/db.js
@@ -1,7 +1,16 @@
+// //codigo pra rodar  servidor local no pc e online no deploy 
+// if(process.env.NODE_ENV == "production"){
+//     module.exports = {mongoURI:"mongodb+srv://marcosluznoble:yHPYPp7sXU6HLd4o@cluster0.rzsq6zh.mongodb.net/blogapp?retryWrites=true&w=majority&appName=Cluster0"}
+// }else{
+//     //module.exports = {mongoURI:"mongodb://localhost/blogapp"}
+//     module.exports = {mongoURI:"mongodb+srv://marcosluznoble:yHPYPp7sXU6HLd4o@cluster0.rzsq6zh.mongodb.net/blogapp?retryWrites=true&w=majority&appName=Cluster0"}
+// }
+
+
 //codigo pra rodar  servidor local no pc e online no deploy 
 if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI:"mongodb+srv://marcosluznoble:yHPYPp7sXU6HLd4o@cluster0.rzsq6zh.mongodb.net/blogapp?retryWrites=true&w=majority&appName=Cluster0"}
+    module.exports = {mongoURI:process.env.MONGOSENHA}
 }else{
     //module.exports = {mongoURI:"mongodb://localhost/blogapp"}
-    module.exports = {mongoURI:"mongodb+srv://marcosluznoble:yHPYPp7sXU6HLd4o@cluster0.rzsq6zh.mongodb.net/blogapp?retryWrites=true&w=majority&appName=Cluster0"}
+    module.exports = {mongoURI:process.env.MONGOSENHA}
 }
